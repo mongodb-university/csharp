342e2516d70d5bdecc23faf431969d0d7fc3b15f
Parker Faucher <parkerfaucher94@gmail.com>
2022-09-30 11:29:03 -0700

    fixed models again

--- Snapshots/inserting-csharp/01-InsertOne/Solved/Models/Account.cs
+++ Snapshots/inserting-csharp/01-InsertOne/Solved/Models/Account.cs
@@ -8,7 +8,7 @@ namespace mongodb_classes.Models
     {
         [BsonId]
         [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
-        public string Id { get; set; } = string.Empty; = string.Empty;
+        public string Id { get; set; } = string.Empty;
         [BsonElement("account_id")]
         public string AccountId { get; set; } = String.Empty;
         [BsonElement("account_holder")]
