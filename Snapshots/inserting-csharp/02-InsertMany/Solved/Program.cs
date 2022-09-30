54cea2a507c76058b1ef58b2f9eb39d9e7fb9491
Parker Faucher <parkerfaucher94@gmail.com>
2022-09-30 09:56:22 -0700

    fixed program.cs

--- Snapshots/inserting-csharp/02-InsertMany/Solved/Program.cs
+++ Snapshots/inserting-csharp/02-InsertMany/Solved/Program.cs
@@ -1,11 +1,14 @@
 using MongoDB.Bson;
-using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Serializers;
 using MongoDB.Driver;
 using mongodb_classes.Models;
 
 BsonSerializer.RegisterSerializer(new DecimalSerializer(BsonType.Decimal128));
 
-var settings = MongoClientSettings.FromConnectionString("");
+DotNetEnv.Env.TraversePath().Load();
+var connectionString = Environment.GetEnvironmentVariable("MONGODB_URI");
+var settings = MongoClientSettings.FromConnectionString(connectionString);
 var client = new MongoClient(settings);
 var database = client.GetDatabase("bank");
 var accountsCollection = database.GetCollection<Account>("accounts");
